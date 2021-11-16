08aa02fd7d12a8abc44e4f62cf15d63bed441017
L37sg0 <petar_1v@abv.bg>
2021-11-16 14:45:10 -0500

    - changing between pages in navigation sets proper nav-link to active

--- nav.php
+++ nav.php
@@ -1,4 +1,78 @@
+
+<?php
+switch ($page) {
+    case 'Page 2':
+        echo '<li class="nav-item">
+    <a class="nav-link" href="index.php">Home</a>
+</li>
 <li class="nav-item">
+    <a class="nav-link active" href="page-2.php">Page 2</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-3.php">Page 3</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-4.php">Page 4</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-5.php">Page 5</a>
+</li>';
+        break;
+    case 'Page 3':
+        echo '<li class="nav-item">
+    <a class="nav-link" href="index.php">Home</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-2.php">Page 2</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link active" href="page-3.php">Page 3</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-4.php">Page 4</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-5.php">Page 5</a>
+</li>';
+        break;
+    case 'Page 4':
+        echo '<li class="nav-item">
+    <a class="nav-link" href="index.php">Home</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-2.php">Page 2</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-3.php">Page 3</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link active" href="page-4.php">Page 4</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-5.php">Page 5</a>
+</li>';
+        break;
+    case 'Page 5':
+        echo '<li class="nav-item">
+    <a class="nav-link" href="index.php">Home</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-2.php">Page 2</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-3.php">Page 3</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="page-4.php">Page 4</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link active" href="page-5.php">Page 5</a>
+</li>';
+        break;
+    default:
+
+    case 'Page 3':
+        echo '<li class="nav-item">
     <a class="nav-link active" href="index.php">Home</a>
 </li>
 <li class="nav-item">
@@ -12,4 +86,5 @@
 </li>
 <li class="nav-item">
     <a class="nav-link" href="page-5.php">Page 5</a>
-</li>
+</li>';
+}
